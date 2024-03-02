5b9cd68efd8c901b6b33049b4a576a948ea3c161
cj-davis99 <cole_davis@brown.edu>
2024-03-02 11:02:35 -0500

    slight tweak to documenting of estimate_beta

--- R/estimate_beta.R
+++ R/estimate_beta.R
@@ -1,13 +1,13 @@
 #' My first function
 #'
 #' @param y A n dimensional numerical vector.
-#' @param X A numerical n x K matrix.
+#' @param x A numerical n x K matrix.
 #'
 #' @return A K dimensional numerical vector of coefficients from the linear regression of y on X.
 #' @export
 #'
 #' @examples
 #' myfunc(3, 5)
-estimate_beta <- function(y, X) {
-  return(lm(y ~ X)$coefficients)
+estimate_beta <- function(y, x) {
+  return(lm(y ~ x)$coefficients)
 }
