5a8d222c603d83ef2a58589f876ec36f19ee7eeb
cj-davis99 <cole_davis@brown.edu>
2024-03-03 10:29:49 -0500

    Another attempt to box plot

--- R/my_theme.R
+++ R/my_theme.R
@@ -11,9 +11,7 @@ my_theme <- function(p) {
   new_p <- p + 
     ggplot2::theme_bw() + 
     ggplot2::theme_classic() + 
-    ggplot2::theme(
-        axis.line.y.right = ggplot2::element_line(size=1, color="black"), 
-        axis.line.x.top = ggplot2::element_line(size=1, color="black")
-    )
+    ggplot2::guide_axis(position = "top")
+    ggplot2::guide_axis(position = "right")
   return(new_p)
 }
