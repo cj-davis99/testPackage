dc99e46fca4327fc9b86161ae8611353b66fd3cc
cj-davis99 <cole_davis@brown.edu>
2024-03-03 10:22:44 -0500

    Adding full frame to plot of my_theme

--- R/my_theme.R
+++ R/my_theme.R
@@ -8,6 +8,12 @@
 #' @examples
 #' my_theme(p)
 my_theme <- function(p) {
-  new_p <- p + ggplot2::theme_bw() + ggplot2::theme_classic()
+  new_p <- p + 
+    ggplot2::theme_bw() + 
+    ggplot2::theme_classic() + 
+    ggplot2::theme(
+        axis.line.y.right = ggplot2::element_line(), 
+        axis.line.x.top = ggplot2::element_line()
+    )
   return(new_p)
 }
